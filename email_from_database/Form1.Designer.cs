281b0f562fef2b8aed204956c19bee65ddce9360
jasaga2401 <jasaga@protonmail.com>
2023-11-28 23:43:56 +0000

    Add project files.

---
+++ email_from_database/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace email_from_database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_email = new System.Windows.Forms.Button();
+            this.lbl_email = new System.Windows.Forms.Label();
+            this.lbl_connect_db = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_email
+            // 
+            this.btn_email.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_email.Location = new System.Drawing.Point(259, 165);
+            this.btn_email.Name = "btn_email";
+            this.btn_email.Size = new System.Drawing.Size(274, 100);
+            this.btn_email.TabIndex = 0;
+            this.btn_email.Text = "Send Email";
+            this.btn_email.UseVisualStyleBackColor = true;
+            this.btn_email.Click += new System.EventHandler(this.btn_email_Click);
+            // 
+            // lbl_email
+            // 
+            this.lbl_email.AutoSize = true;
+            this.lbl_email.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_email.Location = new System.Drawing.Point(255, 329);
+            this.lbl_email.Name = "lbl_email";
+            this.lbl_email.Size = new System.Drawing.Size(179, 20);
+            this.lbl_email.TabIndex = 1;
+            this.lbl_email.Text = "Waiting to send emails";
+            // 
+            // lbl_connect_db
+            // 
+            this.lbl_connect_db.AutoSize = true;
+            this.lbl_connect_db.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_connect_db.Location = new System.Drawing.Point(259, 290);
+            this.lbl_connect_db.Name = "lbl_connect_db";
+            this.lbl_connect_db.Size = new System.Drawing.Size(190, 20);
+            this.lbl_connect_db.TabIndex = 2;
+            this.lbl_connect_db.Text = "Waiting to connect to db";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 475);
+            this.Controls.Add(this.lbl_connect_db);
+            this.Controls.Add(this.lbl_email);
+            this.Controls.Add(this.btn_email);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_email;
+        private System.Windows.Forms.Label lbl_email;
+        private System.Windows.Forms.Label lbl_connect_db;
+    }
+}
+
---
+++ email_from_database/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Mail;
+using System.Data.SqlClient;
+
+namespace email_from_database
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_email_Click(object sender, EventArgs e)
+        {
+
+            // Replace with your actual SQL Server connection string
+            string connectionString = "Data Source=Amaze\\SQLEXPRESS;Initial Catalog=email_db;Integrated Security=True";
+
+            // Create a new SqlConnection using the connection string
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    // Open the database connection
+                    connection.Open();
+                    //Console.WriteLine("Connected to the database.");
+                    lbl_connect_db.Text = "Connected to db";
+
+                    // Define your SQL query
+                    string sqlQuery = "SELECT * FROM dbo.tbl_user";
+
+                    // Create a SqlCommand with the query and the connection
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+                        // Execute the query and get a SqlDataReader
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            // Check if there are rows in the result set
+                            if (reader.HasRows)
+                            {
+                                // Iterate through the rows and output data
+                                while (reader.Read())
+                                {
+                                    // Example: assuming you have a column named "ColumnName"
+                                    string recipientEmail = reader["email"].ToString();
+                                    send_email(recipientEmail);
+                                    lbl_email.Text = recipientEmail;
+
+                                    
+                                    //Console.WriteLine($"Value from database: {columnNameValue}");
+                                }
+                            }
+                            else
+                            {
+                                lbl_email.Text = "No emails found!";
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+                finally
+                {
+                    // Ensure the connection is closed, whether an exception occurs or not
+                    if (connection.State == System.Data.ConnectionState.Open)
+                    {
+                        connection.Close();
+                        Console.WriteLine("Connection closed.");
+                        lbl_email.Text = "Emails sent successfully!";
+                    }
+                }
+            }
+
+        }
+
+        private void send_email(string recipientEmail)
+        {
+
+            // Set up the sender's email address and password
+            string senderEmail = "<username>";
+            string senderPassword = "<app password>";            
+            
+            // Create a new MailMessage
+            MailMessage mail = new MailMessage(senderEmail, recipientEmail);
+
+            // Set the subject and body of the email
+            mail.Subject = "From a db email";
+            mail.Body = "This is a test email sent from C#.";
+
+            // Set up the SMTP client
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+            smtpClient.Port = 587; // Use 587 for Gmail
+            smtpClient.Credentials = new NetworkCredential(senderEmail, senderPassword);
+            smtpClient.EnableSsl = true;
+
+            try
+            {
+                // Send the email
+                smtpClient.Send(mail);
+                lbl_email.Text = "Email sent successfully!";
+                //Console.WriteLine("Email sent successfully!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+        }
+    }
+}
